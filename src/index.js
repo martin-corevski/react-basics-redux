0dc8af3569c8d6cb571616a131618a701ddf8b25
Martin Corevski <martin-corevski@outlook.com>
2018-04-09 22:11:47 +0200

    Advanced redux
    Enhancers (middlewares) added
    Async actions with redux-thunk
    Multiple action creators and reducers
    Utility functions for leaner switch cases
    Enabled redux chrome dev tools

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore, combineReducers } from 'redux'
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
+import thunk from 'redux-thunk'
 
 import './index.scss'
 import App from './containers/App'
@@ -12,13 +13,32 @@ import App from './containers/App'
 import counterReducer from './store/reducers/counter'
 import resultReducer from './store/reducers/result'
 
+// Combining multiple reducers
 const rootReducer = combineReducers({
   ctr: counterReducer,
   res: resultReducer
 })
 
+// Custom middleware for logging the dispatched actions and current state
+const logger = store => {
+  return next => {
+    return action => {
+      console.log('[Middleware] Dispatching ', action)
+      const result = next(action)
+      console.log('[Middleware] Next state ', store.getState())
+      return result
+    }
+  }
+}
+
+// For Chrome Dev tools, https://github.com/zalmoxisus/redux-devtools-extension
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+// Single reducer store
 // const store = createStore(reducer)
-const store = createStore(rootReducer)
+// Multiple reducers store and as a second argument we can pass the so called
+// enhancer (the middleware or multiple middlewares)
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger, thunk)))
 
 ReactDOM.render(
   <Provider store={store}>
---
+++ src/store/actions/actionTypes.js
@@ -0,0 +1,8 @@
+// In order to remove or lower the chances of making a typo we are declaring all
+// of the actions as constants
+export const INCREMENT = 'INCREMENT'
+export const DECREMENT = 'DECREMENT'
+export const ADD = 'ADD'
+export const SUBTRACT = 'SUBTRACT'
+export const ADD_RESULT = 'ADD_RESULT'
+export const REMOVE_RESULT = 'REMOVE_RESULT'
